9eb5fa5115ebd060ce99710d7dc37d60689bdf4d
Gunjan Patel <patelgunjan5@gmail.com>
2016-01-29 00:02:32 -0800

    final submitted version

--- README.txt
+++ README.txt
@@ -6,6 +6,47 @@ Date: Jan 28
 
 
 
+
+Test cases: 
+================
+HTTP/1.0, 200 OK
+	telnet localhost 8080
+	GET / HTTP/1.0
+
+HTTP/1.0, 400 BAD REQUEST
+	telnet localhost 8080
+	GET bad HTTP/1.0
+
+HTTP/1.0, 404 NOT FOUND
+	telnet localhost 8080
+	GET /nothing.html HTTP/1.0
+
+HTTP/1.0, 403 FORBIDDEN
+	telnet localhost 8080
+	GET /nopermission.html HTTP/1.0	
+
+================
+HTTP/1.1, 200 OK
+	telnet localhost 8080
+	GET / HTTP/1.0
+
+HTTP/1.1, 400 BAD REQUEST
+
+	GET bad HTTP/1.0
+
+HTTP/1.1, 404 NOT FOUND
+
+	GET /nothing.html HTTP/1.0
+
+HTTP/1.1, 403 FORBIDDEN
+
+	GET /nopermission.html HTTP/1.0	
+
+=================
+Supports HTTP/1.1 and 1.0
+Spins new threads (goroutine) for every new connections 
+
+
 List of files:
 
 ├── Santa Clara University_files
@@ -16,11 +57,20 @@ List of files:
 │   ├── scu.css
 │   ├── search-autocomplete.js
 │   └── sinskywchild760480.jpg
-├── eindex.html
+├── nopermission.html			//set it to no read permission (sudo chmod 200 nopermission.html)
 ├── index.html
 ├── minion.gif
 ├── r2d2.jpg
 ├── server
 └── server.go
+script.PDF
 
 1 directory, 42 files
+
+
+
+Instructions for running:
+-This assignment is written in Google's new programming language Go aka Golang (with professor's prior permission)
+-Tested with Go version 1.5
+-makefile is provided 
+-If you want to test it and don't have Go installed, use this link where I have this project setup with Go installed in the cloud: https://ide.c9.io/gunjan5/coen317-assignment1
--- server.go
+++ server.go
@@ -1,123 +1,79 @@
+//Name: Gunjan Patel
+//COEN317 assignment 1
+
 package main
 
 import (
-	"fmt"
-	"net/http"
-	_"io"
-	"os"
 	"flag"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"os"
 	"strconv"
+	"time"
 )
 
-// func handler(w http.ResponseWriter, r *http.Request) {
-// 	io.WriteString(w, "Hello world!")
-// }
-
-// func testhandler(w http.ResponseWriter, r *http.Request) {
-// 	io.WriteString(w, "TEST!!!!!!!!!!!!!!!!!!!!!!! !")
-// }
+const (
+	ReadTime  = 60
+	WriteTime = 90
+)
 
 func main() {
-	//var p int
+	//Starting a new multiplexer/Router to route requests
+	mux := http.NewServeMux()
 
+	//Getting current dir for default directory_root (if not specifies)
+	wd, _ := os.Getwd()
 
-	mux:= http.NewServeMux()
-	wd,_:=os.Getwd()
+	//specifying the flags
 	dir := flag.String("document_root", wd, "document root")
 	port := flag.Int("port", 8080, "specify server listen port")
+
+	//parsing the flags
 	flag.Parse()
-	
+
+	//making sure the document_root specified exists, if not then exit with error
 	if _, err := os.Stat(*dir); os.IsNotExist(err) {
-  fmt.Println("ERROR: File/path does not exist! Exiting the program ...")
-  os.Exit(1)
-}
+		fmt.Println("ERROR: File/path does not exist! Exiting the program ...")
+		os.Exit(1)
+	}
 
-
-	fileRoot:=http.Dir(*dir)
+	//ser file root from the flag
+	fileRoot := http.Dir(*dir)
 	//redirectHandler:= http.RedirectHandler(":8080/index.html", 307)
-	files:=http.FileServer(fileRoot)
+
+	//FileServer will return a handler
+	files := http.FileServer(fileRoot)
 	//http.HandleFunc("/test", testhandler)
+
+	//specify the handler for root url
 	mux.Handle("/", files)
 	//fmt.Println(*port)
-	srvAddr := ":"+strconv.Itoa(*port)
+	srvAddr := ":" + strconv.Itoa(*port)
 	//fmt.Printf("%T\n\n\n", srvAddr)
 	fmt.Println(srvAddr)
 	fmt.Println(fileRoot)
-		
-	http.ListenAndServe(srvAddr, mux)
+
+	//Set server properties: address:port, handler, Read/Write timeouts (note these will also defer based on HTTP/1.0 and HTTP/1.1, this is maximum)
+	srv := &http.Server{
+		Addr:         srvAddr,
+		Handler:      mux,
+		ReadTimeout:  ReadTime * time.Second,
+		WriteTimeout: WriteTime * time.Second,
+	}
+
+	//Listen on TCP port specified, if not then default port 8080 and localhost
+	ln, err := net.Listen("tcp", srvAddr)
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+
+	//Close the listener when done
+	defer ln.Close()
+
+	//Serve the server once server gets a connection and Listener is returned
+	srv.Serve(ln) //Serve method spins threads (goroutines) for every new connections
+
+	//srv.ListenAndServe()
 }
-
-
-
-
-
-
-
-
-
-// func main() {
-// 	log.Fatal(http.ListenAndServe(":8080", http.FileServer(http.Dir("/Volumes/Other/desktop/go30days-challenge/day29-web-server/"))))
-
-// }
-
-
-
-
-
-
-
-// import (
-// 	"bufio"
-// 	"fmt"
-// 	"io"
-// 	"log"
-// 	"net"
-// 	"strings"
-// )
-
-// func handleConn(conn net.Conn) {
-// 	//defer conn.Close()
-// 	scanner := bufio.NewScanner(conn)
-// 	i := 0
-// 	for scanner.Scan() {
-// 		ln := scanner.Text()
-// 		fmt.Println(ln)
-
-// 		if i == 0 {
-// 			method := strings.Fields(ln)[0]
-// 			fmt.Println("METHOD", method)
-// 		} else {
-// 			// in headers now
-// 			// when line is empty, header is done
-// 			if ln == "" {
-// 				break
-// 			}
-// 		}
-
-// 		i++
-// 	}
-
-// 	// response
-// 	body := "hello world 2"
-
-// 	io.WriteString(conn, "HTTP/1.1 200 OK\r\n")
-// 	fmt.Fprintf(conn, "Content-Length: %d\r\n", len(body))
-// 	io.WriteString(conn, "\r\n")
-// 	io.WriteString(conn, body)
-// }
-
-// func main() {
-// 	server, err := net.Listen("tcp", ":9000")
-// 	if err != nil {
-// 		log.Fatalln(err.Error())
-// 	}
-// 	defer server.Close()
-
-// 	for {
-// 		conn, err := server.Accept()
-// 		if err != nil {
-// 			log.Fatalln(err.Error())
-// 		}
-// 		go handleConn(conn)
-// 	}
-// }
